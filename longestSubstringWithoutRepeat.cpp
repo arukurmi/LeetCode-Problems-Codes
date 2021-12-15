54b08012e3bf06ed2d362123d7561df3681a1e45
Aryansh <2018eeb1141@iitrpr.ac.in>
2021-12-15 22:40:11 +0530

    code add

--- longestSubstringWithoutRepeat.cpp
+++ longestSubstringWithoutRepeat.cpp
@@ -1,3 +1,13 @@
+// Given a string, 
+
+// find the length of the longest substring without repeating characters.
+
+// Example:
+
+// The longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3.
+
+// For "bbbbb" the longest substring is "b", with the length of 1.
+
 int Solution::lengthOfLongestSubstring(string s){
 
     int ans = INT_MIN;
