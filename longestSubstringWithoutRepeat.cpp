60eafc8f49c2c25ab3c0fe4eb16a446fc2b9d6dc
Aryansh <2018eeb1141@iitrpr.ac.in>
2021-12-15 22:39:05 +0530

    code add

--- longestSubstringWithoutRepeat.cpp
+++ longestSubstringWithoutRepeat.cpp
@@ -1,21 +1,26 @@
-shdjlfdsl
-sljlfhl
-sdjfhdsl
-shdjlfdsl
-sljlfhl
-sdjfhdsl
-shdjlfdsl
-sljlfhl
-sdjfhdsl
-shdjlfdsl
-sljlfhl
-sdjfhdsl
-shdjlfdsl
-sljlfhl
-sdjfhdsl
-shdjlfdsl
-sljlfhl
-sdjfhdsl
-shdjlfdsl
-sljlfhl
-sdjfhdsl
+int Solution::lengthOfLongestSubstring(string s){
+
+    int ans = INT_MIN;
+    int len = s.length();
+    map<char, int> mp;
+    int temp = 0;
+    int l = 0, r = 0, winlen = 0;
+    while(r<len){
+        char temp = s[r];
+
+        if(mp.find(temp) != mp.end()){
+            while(mp[temp]){
+                mp[s[l]]--;
+                l++;
+            }
+        }
+        
+        mp[temp]++;
+        winlen = r-l+1;
+        ans = max(winlen, ans);
+        r++;
+
+    }
+
+    return ans;
+}
