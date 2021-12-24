3b3353604feabf29724aa7687f97f340a16222a2
Aryansh <2018eeb1141@iitrpr.ac.in>
2021-12-24 20:38:51 +0530

    NQueens

--- NQueens.cpp
+++ NQueens.cpp
@@ -21,3 +21,26 @@
 //   "...Q",
 //   ".Q.."]
 // ]
+
+#include<stdio.h>
+using namespace std;
+
+
+vector<vector<string> > Solution::solveNQueens(int n){
+
+    vector<vector<string>> v;
+    vector<string> board(n);
+    string s(n, '.');
+
+    for(int i=0; i<n; i++){
+        board[i] = s;
+    }
+
+    vector<int> left(n, 0);
+    vector<int> upperdia(2*n-1, 0);
+    vector<int> lowerdia(2*n-1, 0);
+
+    solve(left, upperdia, lowerdia, board, v, 0, n);
+    
+    return v;
+}
