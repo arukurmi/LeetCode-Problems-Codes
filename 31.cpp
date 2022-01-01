2039ba7bf2ffd60a7b5347471230e422deb08b1f
Aryansh <2018eeb1141@iitrpr.ac.in>
2022-01-01 19:36:24 +0530

    leetcode 31 solution

--- 31.cpp
+++ 31.cpp
@@ -3,3 +3,28 @@
 // The replacement must be in place and use only constant extra memory.
 
 
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int lastMin = n - 2;
+        
+        while(lastMin>=0 && nums[lastMin] >= nums[lastMin+1]){
+            lastMin--;
+        }
+        
+        if(lastMin < 0)
+            reverse(nums.begin() , nums.end());
+        
+        else{
+            int j = n-1;
+            while(j>=0 && nums[j] <= nums[lastMin]){
+                j--;
+            }
+            swap(nums[j] , nums [lastMin]);
+            reverse(nums.begin() + lastMin + 1 , nums.end());
+        }
+        
+        return;        
+    }
+};
