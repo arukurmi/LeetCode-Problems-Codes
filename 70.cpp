4a1469d1ce4d53e253dcfe17a82d0a9a904a63d2
Aryansh <2018eeb1141@iitrpr.ac.in>
2022-01-11 22:55:44 +0530

    leetcode 70 solution

--- 70.cpp
+++ 70.cpp
@@ -1,5 +1,25 @@
-You are climbing a staircase. It takes n steps to reach the top.
+// You are climbing a staircase. It takes n steps to reach the top.
 
-Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
- 
+ class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1 || n==2 || n==3)
+            return n;
+        
+        int dp[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        
+        for(int i=4; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+//         if(n==3){
+            
+//         }
+        return dp[n];
+        
+    }
+};
