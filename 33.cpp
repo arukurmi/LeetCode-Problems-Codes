1e4473676c015977618603ad4790b43e92161d57
Aryansh <2018eeb1141@iitrpr.ac.in>
2022-01-12 21:33:07 +0530

    leetcode 33 solution

--- 33.cpp
+++ 33.cpp
@@ -7,4 +7,15 @@
 // You must write an algorithm with O(log n) runtime complexity.
 
 
-
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == target)
+                return i;
+        }
+        
+        return -1;
+    }
+};
