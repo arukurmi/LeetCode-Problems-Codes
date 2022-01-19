13552f9d7295886108e809bfa5b7befb2a969717
Aryansh <2018eeb1141@iitrpr.ac.in>
2022-01-20 01:00:59 +0530

    leetcode 35 solution

--- 35.cpp
+++ 35.cpp
@@ -1,6 +1,6 @@
 // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-
 // You must write an algorithm with O(log n) runtime complexity.
+
 
 class Solution {
 public:
@@ -9,7 +9,7 @@ public:
         int n = v.size();
         int s = 0, e = n-1;
         int mid;
-        
+
         while(s<=e){
             
             mid = s+(e-s)/2;
